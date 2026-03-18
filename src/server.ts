b6ae7bec1ea1d50111c89abdac4658c235fbfd0c
Kaizen <gibrantsyareza@gmail.com>
2026-03-18 15:40:22 +0700

    root of the project, it will all start through here.

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import App from "./app";
+import "dotenv/config"
+const PORT = process.env.PORT;
+
+const initializeApp = async () => {
+    try{
+        App.listen(PORT, () => {
+            console.log(
+               `[server]: server is running at http://localhost:${PORT}`
+            );
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1)
+    }
+}
+
+initializeApp()
