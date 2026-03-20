453770fc5b3c1f7d2c9d54ee0a63c7ab5beba09e
Kaizen <gibrantsyareza@gmail.com>
2026-03-21 00:26:20 +0700

    implemanting permission validation and auth middleware

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -2,14 +2,46 @@ import { Router } from "express";
 import UserController from "../controllers/UserController.js";
 import ValidateRequest from "../middlewares/ValidateRequest.js";
 import { createUserSchema, updateUserSchema } from "../Validations/UserValidation.js";
+import Auth from "../middlewares/Auth.js";
+import { Permission } from "../configs/permission.js";
 
 const router = Router();
 const userController = new UserController();
+const auth = new Auth();
 
-router.get("/", userController.findAll.bind(userController));
-router.get("/:id", userController.findById.bind(userController));
-router.post("/", ValidateRequest(createUserSchema),userController.create.bind(userController));
-router.put("/:id", ValidateRequest(updateUserSchema), userController.update.bind(userController));
-router.delete("/:id", userController.delete.bind(userController));
+// Get all users - ADMIN only
+router.get(
+  "/",
+  auth.verifyPermission(Permission.READ_ALL_USERS),
+  userController.findAll.bind(userController)
+);
 
-export { router as UserRoutes } ;
+// Get user by ID - bisa user atau admin
+router.get(
+  "/:id",
+  userController.findById.bind(userController)
+);
+
+// Create user - ADMIN only
+router.post(
+  "/",
+  auth.verifyPermission(Permission.CREATE_USER),
+  ValidateRequest(createUserSchema),
+  userController.create.bind(userController)
+);
+
+// Update user - bisa user atau admin
+router.put(
+  "/:id",
+  ValidateRequest(updateUserSchema),
+  userController.update.bind(userController)
+);
+
+// Delete user - ADMIN only
+router.delete(
+  "/:id",
+  auth.verifyPermission(Permission.DELETE_ANY_USER),
+  userController.delete.bind(userController)
+);
+
+export { router as UserRoutes };
