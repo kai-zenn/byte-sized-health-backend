e6fc2217c8f1c6c935197ee45747187d4bca5219
Kaizen <gibrantsyareza@gmail.com>
2026-04-05 14:38:59 +0700

    Add utility functions for sanitizing HTML and generating slugs

---
+++ src/utils/SanitizeUtil.ts
@@ -0,0 +1,10 @@
+export function sanitizeHtml(html: string): string {
+  // remove dangerous tags/attributes
+  // For production, will use library like 'sanitize-html'
+
+  return html
+    .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
+    .replace(/<iframe\b[^<]*(?:(?!<\/iframe>)<[^<]*)*<\/iframe>/gi, '')
+    .replace(/on\w+="[^"]*"/gi, '')
+    .replace(/on\w+='[^']*'/gi, '');
+}
