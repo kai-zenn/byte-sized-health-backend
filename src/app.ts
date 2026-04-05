efce1371438fc4059a33f678e14d74f5f27cd179
Kaizen <gibrantsyareza@gmail.com>
2026-04-05 14:43:41 +0700

    Serve static files from the /uploads directory

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 // import helmet from "helmet";
 // import morgan from "morgan";
+import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -28,6 +29,9 @@ App.use(express.urlencoded({ extended: true }));
 // } else {
 //   App.use(morgan("combined")); // or use pino/winston in production
 // }
+
+// Serve static files (uploaded images)
+App.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 /* Routes */
 App.use("/api", AppRoutes)
