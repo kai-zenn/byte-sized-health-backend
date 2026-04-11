5b4b7bcb0be9597602c1e42a502e9cbec9cc184c
Kaizen <gibrantsyareza@gmail.com>
2026-04-12 00:57:25 +0700

    updating AppRoutes.ts to include the endpoint for accessing the LLM

--- src/routes/main/AppRoutes.ts
+++ src/routes/main/AppRoutes.ts
@@ -3,6 +3,7 @@ import { UserRoutes } from "../UserRoute.js";
 import { AuthRoutes } from "../AuthRoutes.js";
 import { ArticleRoutes } from "../ArticlesRoutes.js";
 import Auth from "../../middlewares/Auth.js";
+import { LlmRoutes } from "../LlmRoutes.js";
 
 const router = Router();
 const authMiddleware = new Auth();
@@ -17,6 +18,10 @@ router.use(
   UserRoutes
 );
 
+// Articles routes for pubblic access
 router.use("/articles", ArticleRoutes);
+
+// LLM integration routes
+router.use("/llm", LlmRoutes);
 
 export { router as AppRoutes };
